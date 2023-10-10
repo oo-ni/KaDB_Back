a156a7b4a995ff1c646d1f0291a8617c52d0b32b
asasia1935 <sungmin1935@naver.com>
2023-10-10 12:08:33 +0900

    KSM 20231010 Create Back Project

---
+++ KaDB-Backend/src/main/java/com/example/kadbbackend/KaDbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kadbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaDbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaDbBackendApplication.class, args);
+	}
+
+}
---
+++ KaDB-Backend/src/test/java/com/example/kadbbackend/KaDbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kadbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaDbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
