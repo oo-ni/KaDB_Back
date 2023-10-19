a15751de86bdd6c43753b073a8e62cc484e64594
asasia1935 <sungmin1935@naver.com>
2023-10-19 16:26:14 +0900

    KSM 2023/10/19 add Eureka, add Memberservice(User.class)
    기본 설정 추가 및 멤버 서비스 추가

---
+++ member-service/src/main/java/com/example/memberservice/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.example.memberservice;
+
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+    @CreationTimestamp
+    private LocalDateTime createdTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedTime;
+
+    @ColumnDefault("true")
+    private Boolean deleted;
+}
